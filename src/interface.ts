9e93d86df4c67042b04c2cbcbee7ebea57ab5ee3
wataru <dossjakapoko@gmail.com>
2020-06-13 15:49:14 +0900

    interfaceを関数で定義する

--- src/interface.ts
+++ src/interface.ts
@@ -1,10 +1,17 @@
-// extendするときは継承する前のinterfaceに含まれてる型が、継承した後の型に代入できる限り上書きできる
-interface Nameable {
-  name: string;
+// type addFunc = (num1: number, num2: number) => number;
+interface addfunc {
+  (num1: number, num2: number): number;
 }
 
-interface Human extends Nameable {
+let addfunc: addFunc;
+addfunc = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+type Nameable = {
   name: string;
+};
+
+interface Human extends Nameable {
   age: number;
   greeting(message: string): void;
 }
