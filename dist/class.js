869f2ba28f73c54cf89b9e014f97453bc4c77ecf
wataru <dossjakapoko@gmail.com>
2020-06-13 12:06:48 +0900

    protect, get, set

--- dist/class.js
+++ dist/class.js
@@ -1,12 +1,21 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Parson = /** @class */ (function () {
     function Parson(name, age) {
         this.name = name;
         this.age = age;
-        this.id = 32;
-        this.id = 31;
-        this.name = "hello";
-        this.id = 30;
     }
     Parson.prototype.incrementAge = function () {
         this.age += 1;
@@ -16,10 +25,36 @@ var Parson = /** @class */ (function () {
     };
     return Parson;
 }());
-var person2;
-var quill = new Parson("Quill", 35);
-quill.incrementAge();
-console.log(quill.id);
-quill.greeting();
+var Teacher = /** @class */ (function (_super) {
+    __extends(Teacher, _super);
+    function Teacher(name, age, _subject) {
+        var _this = _super.call(this, name, age) || this;
+        _this._subject = _subject;
+        return _this;
+    }
+    Object.defineProperty(Teacher.prototype, "subject", {
+        get: function () {
+            if (!this._subject) {
+                throw new Error("there is no subject.");
+            }
+            return this._subject;
+        },
+        set: function (value) {
+            if (!this._subject) {
+                throw new Error("there is no subject.");
+            }
+            this._subject = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Teacher.prototype.greeting = function () {
+        console.log("Hello My name is " + this.name + ", " + this.age + " years old.");
+    };
+    return Teacher;
+}(Parson));
+var teacher = new Teacher("Qiill", 38, "math");
+teacher.subject = "Music";
+console.log(teacher.subject);
+teacher.greeting();
 // コンソール
-// Hello My name is Quill, 36 years old.
--- src/class.ts
+++ src/class.ts
@@ -1,5 +1,5 @@
 class Parson {
-  constructor(public name: string, private age: number) {}
+  constructor(public name: string, protected age: number) {}
   incrementAge() {
     this.age += 1;
   }
@@ -10,19 +10,30 @@ class Parson {
 }
 
 class Teacher extends Parson {
-  constructor(name: string, age: number, public sublect: string) {
+  get subject() {
+    if (!this._subject) {
+      throw new Error("there is no subject.");
+    }
+    return this._subject;
+  }
+
+  set subject(value) {
+    if (!value) {
+      throw new Error("there is no subject.");
+    }
+    this._subject = value;
+  }
+  constructor(name: string, age: number, private _subject: string) {
     super(name, age);
   }
 
   greeting(this: Teacher) {
     console.log(`Hello My name is ${this.name}, ${this.age} years old.`);
-    // .ageにエラー
-    // Parson.age: number
-    //   プロパティ 'age' はプライベートで、クラス 'Parson' 内でのみアクセスできます
   }
 }
 const teacher = new Teacher("Qiill", 38, "math");
-
+teacher.subject = "Music";
+console.log(teacher.subject);
 teacher.greeting();
 
 // コンソール
