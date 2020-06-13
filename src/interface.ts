7bf7a2fa1c4b13dc129b32ff7397f344589a4e84
wataru <dossjakapoko@gmail.com>
2020-06-13 15:40:25 +0900

    lerned extend to interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,10 +1,15 @@
-interface Human {
-  readonly name: string;
+// extendするときは継承する前のinterfaceに含まれてる型が、継承した後の型に代入できる限り上書きできる
+interface Nameable {
+  name: string;
+}
+
+interface Human extends Nameable {
+  name: string;
   age: number;
   greeting(message: string): void;
 }
 
-class Developer implements Human {
+class Developer implements Human, Nameable {
   constructor(
     public name: string,
     public age: number,
@@ -23,7 +28,3 @@ const tmpDeveloper = {
   },
 };
 const user: Human = tmpDeveloper;
-let developer = new Developer("Quill", 34, 3);
-developer.name = "hello";
-// クラスを経由してるため、エラーにならない
-// implementsの中のreadonlyはクラスの中には影響しない
