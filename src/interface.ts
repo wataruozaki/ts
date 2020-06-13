fe0248c20669f2e5799c0f7a9840771d58ab3053
wataru <dossjakapoko@gmail.com>
2020-06-13 14:58:32 +0900

    learned  implements

--- src/interface.ts
+++ src/interface.ts
@@ -1,14 +1,18 @@
+// クラスとインターフェイスを同時に使う
+// インターフェイスの内容をクラスに対して指定したい時
 interface Human {
   name: string;
   age: number;
   greeting(message: string): void;
-  //メソッドのみこの書き方ができる。関数にはできない
 }
 
-const human: Human = {
-  name: "Quill",
-  age: 38,
+class Developer implements Human {
+  constructor(
+    public name: string,
+    public age: number,
+    public experience: number
+  ) {}
   greeting(message: string) {
-    console.log(message);
-  },
-};
+    console.log("Hello!");
+  }
+}
