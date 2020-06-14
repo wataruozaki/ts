ac056c1cd6ee1d9d73705605615b4addccb67033
wataru <dossjakapoko@gmail.com>
2020-06-14 16:29:54 +0900

    戻り値に指定して、新しいクラスを作成する

---
+++ dist/decorator.js
@@ -0,0 +1,48 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// デコレーターファクトリー
+// 勝手に定義できないのでデコレーターを返す関数をかく
+function Logging(message) {
+    return function (constructor) {
+        console.log(message);
+        console.log(constructor);
+    };
+}
+function Component(template, selector) {
+    return function (
+    // ここに指定される
+    constructor) {
+        //class Userはしたの無名クラスに書き換わってえる
+        return class extends constructor {
+            constructor(...args) {
+                super(...args);
+                console.log("Component");
+                const mountedElement = document.querySelector(selector);
+                const instance = new constructor();
+                if (mountedElement) {
+                    mountedElement.innerHTML = template;
+                    mountedElement.querySelector("h1").textContent = instance.name;
+                }
+            }
+        };
+    };
+}
+let User = class User {
+    constructor(age) {
+        this.age = age;
+        this.name = "Quill";
+        console.log("User was created!");
+    }
+};
+User = __decorate([
+    Logging("Logging User"),
+    Component("<h1> {{ name }} </h1>", "#app")
+], User);
+const user1 = new User(32);
+const user2 = new User(32);
+const user3 = new User(32);
---
+++ src/decorator.ts
@@ -0,0 +1,42 @@
+// デコレーターファクトリー
+// 勝手に定義できないのでデコレーターを返す関数をかく
+function Logging(message: string) {
+  return function (constructor: Function) {
+    console.log(message);
+    console.log(constructor);
+  };
+}
+
+function Component(template: string, selector: string) {
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    // ここに指定される
+    constructor: T
+  ) {
+    //class Userはしたの無名クラスに書き換わってえる
+    return class extends constructor {
+      constructor(...args: any[]) {
+        super(...args);
+        console.log("Component");
+        const mountedElement = document.querySelector(selector);
+        const instance = new constructor();
+        if (mountedElement) {
+          mountedElement.innerHTML = template;
+          mountedElement.querySelector("h1")!.textContent = instance.name;
+        }
+      }
+    };
+  };
+}
+
+@Logging("Logging User")
+@Component("<h1> {{ name }} </h1>", "#app")
+class User {
+  name = "Quill";
+  constructor(public age: number) {
+    console.log("User was created!");
+  }
+}
+
+const user1 = new User(32);
+const user2 = new User(32);
+const user3 = new User(32);
