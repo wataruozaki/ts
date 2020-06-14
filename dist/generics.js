8092a9e3b52d20f602489320c523f8467132080c
wataru <dossjakapoko@gmail.com>
2020-06-14 13:49:35 +0900

    classにジェネリクスを使う

--- dist/generics.js
+++ dist/generics.js
@@ -4,3 +4,27 @@ function copy(value, key) {
     return value;
 }
 console.log(copy({ name: "Quill", age: 38 }, "name"));
+var LightDatabase = /** @class */ (function () {
+    function LightDatabase() {
+        this.data = [];
+    }
+    LightDatabase.prototype.add = function (item) {
+        this.data.push(item);
+    };
+    LightDatabase.prototype.remove = function (item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    };
+    LightDatabase.prototype.get = function () {
+        return this.data;
+    };
+    return LightDatabase;
+}());
+var stringLightDatabase = new LightDatabase();
+// ここでstringsと決めたらstring
+stringLightDatabase.add("apple");
+stringLightDatabase.add("banana");
+stringLightDatabase.add("grape");
+stringLightDatabase.remove("banana");
+console.log(stringLightDatabase.get());
+// コンソール
+// ['apple', 'grape']
--- src/generics.ts
+++ src/generics.ts
@@ -6,3 +6,26 @@ function copy<T extends { name: string }, U extends keyof T>(
   return value;
 }
 console.log(copy({ name: "Quill", age: 38 }, "name"));
+
+class LightDatabase<T extends string | number | boolean> {
+  private data: T[] = [];
+  add(item: T) {
+    this.data.push(item);
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  get() {
+    return this.data;
+  }
+}
+const stringLightDatabase = new LightDatabase<string>();
+// ここでstringsと決めたらstring
+stringLightDatabase.add("apple");
+stringLightDatabase.add("banana");
+stringLightDatabase.add("grape");
+stringLightDatabase.remove("banana");
+
+console.log(stringLightDatabase.get());
+// コンソール
+// ['apple', 'grape']
