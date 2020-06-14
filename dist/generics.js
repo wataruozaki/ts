22cd780801fb5ce87132afc768c44326c1a17cdc
wataru <dossjakapoko@gmail.com>
2020-06-14 13:26:04 +0900

    extend

--- dist/generics.js
+++ dist/generics.js
@@ -1,8 +1,6 @@
 "use strict";
-// ジェネリクスとは - 型を引数で受け取る仕組み
 function copy(value) {
     return value;
 }
-// console.log(copy<string>("hello"));
 console.log(copy({ name: "Quill" }));
-// 関数にジェネリクスをしているので、型推論してくれる
+// ジェネリクスにおけるextendは狭めるイメージ;
--- src/generics.ts
+++ src/generics.ts
@@ -1,8 +1,6 @@
-// ジェネリクスとは - 型を引数で受け取る仕組み
-
-function copy<T>(value: T): T {
+function copy<T extends { name: string }>(value: T): T {
   return value;
 }
-// console.log(copy<string>("hello"));
 console.log(copy({ name: "Quill" }));
-// 関数にジェネリクスをしているので、型推論してくれる
+
+// ジェネリクスにおけるextendは狭めるイメージ;
