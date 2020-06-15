9835a474643e4d3f761d4e9b8823624c23726e35
wataru <dossjakapoko@gmail.com>
2020-06-15 13:52:53 +0900

    ファイルのモジュール分割

--- dist/foods.js
+++ dist/foods.js
@@ -1,32 +1,5 @@
-"use strict";
-class Score {
-    constructor() { }
-    get totalScore() {
-        const foods = Foods.getInstance();
-        return foods.activeElementsScore.reduce((total, score) => total + score, 0);
-    }
-    render() {
-        document.querySelector(".score__number").textContent = String(this.totalScore);
-    }
-    static getInstance() {
-        if (!Score.instance) {
-            Score.instance = new Score();
-        }
-        return Score.instance;
-    }
-}
-class Food {
-    constructor(element) {
-        this.element = element;
-        element.addEventListener("click", this.clickEventHandler.bind(this));
-    }
-    clickEventHandler() {
-        this.element.classList.toggle("food--active");
-        const score = Score.getInstance();
-        score.render();
-    }
-}
-class Foods {
+import { Food } from "./food.js";
+export class Foods {
     constructor() {
         this.elements = document.querySelectorAll(".food");
         this._activeElements = [];
@@ -61,4 +34,3 @@ class Foods {
         return Foods.instance;
     }
 }
-const foods = Foods.getInstance();
